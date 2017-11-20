ddac1a27dd4f2031625620d567cdb01df5bb7ee6
Taylor Sullivan <taylor.sullivan@me.com>
2017-11-19 19:30:02 -0800

    Add option to select latest release

--- bin/wow/delete_addon.rb
+++ bin/wow/delete_addon.rb
@@ -7,11 +7,11 @@ $ADDONS_PATH = '/Applications/World of Warcraft/Interface/AddOns'
 def run
   addon_name = ARGV[0]
 
-  print 'Deleting local install, '
+  print 'Deleting local install… '
   `rm -rf #{ Shellwords.escape(File.join(Dir.pwd, addon_name)) }`
   puts 'done.'
 
-  print "Deleting link in #{ $ADDONS_PATH }, "
+  print "Deleting link in #{ $ADDONS_PATH }… "
   `rm #{ Shellwords.escape(File.join($ADDONS_PATH, addon_name)) }`
   puts 'done.'
 end
